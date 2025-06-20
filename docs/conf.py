0486e786b8fc42bc93750079c56bf0764a402795
LisaLuice <lisaluice778@gmail.com>
2025-06-20 11:38:41 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 html_theme = 'alabaster'
-html_static_path = ['static'] 
+html_static_path = ['_static'] 
 
 def setup(app):
     app.add_js_file("chatbot.js") 
