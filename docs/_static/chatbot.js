7a95556c71718ca2244160d2363894a8af70746e
LisaLuice <lisaluice778@gmail.com>
2025-06-20 11:54:40 +0530

    Create chatbot.js

---
+++ docs/_static/chatbot.js
@@ -0,0 +1,6 @@
+(function() {
+  var ze = document.createElement('script');
+  ze.id = 'ze-snippet';
+  ze.src = 'https://static.zdassets.com/ekr/snippet.js?key=YOUR_KEY';
+  document.body.appendChild(ze);
+})();
